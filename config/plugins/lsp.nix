76604ece8ed2277ed7b0bf9f586a575d14c89d50
blank2121 <winstonwalter07@gmail.com>
2024-03-14 19:02:44 -0400

    changed CMP to new update and now luasnip works!

--- config/plugins/lsp.nix
+++ config/plugins/lsp.nix
@@ -26,41 +26,32 @@
   plugins.luasnip.enable = true;
   plugins.cmp-nvim-lsp.enable = true;
 
-  plugins.nvim-cmp = {
+  plugins.cmp = {
     enable = true;
     autoEnableSources = true;
-    sources = [
-      { name = "nvim_lsp"; }
-      { name = "luasnip"; }
-      { name = "path"; }
-      { name = "buffer"; }
-    ];
-    mapping = {
-      "<C-Space>" = "cmp.mapping.complete()";
-      "<C-d>" = "cmp.mapping.scroll_docs(-4)";
-      "<C-e>" = "cmp.mapping.close()";
-      "<C-f>" = "cmp.mapping.scroll_docs(4)";
-      "<CR>" = {
-      	action = "cmp.mapping.confirm({ select = true })"; 
-	modes = [
-	  "i"
-	  "s"
-	];
+    settings = {
+      sources = [
+        { name = "nvim_lsp"; }
+        { name = "luasnip"; }
+        { name = "path"; }
+        { name = "buffer"; }
+      ];
+      mapping = {
+        "<C-Space>" = "cmp.mapping.complete()";
+        "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+        "<C-e>" = "cmp.mapping.close()";
+        "<C-f>" = "cmp.mapping.scroll_docs(4)";
+        "<CR>" = "cmp.mapping.confirm({ select = true })"; 
+	"<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+  	"<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
       };
-      "<S-Tab>" = {
-        action = "cmp.mapping.select_prev_item()";
-        modes = [
-          "i"
-          "s"
-        ];
-      };
-      "<Tab>" = {
-        action = "cmp.mapping.select_next_item()";
-        modes = [
-          "i"
-          "s"
-        ];
-      };
-    };
+
+      snippet.expand = ''
+	function(args)
+	  require("luasnip").lsp_expand(args.body)
+	end
+      '';
+
+    }; 
   };
 }
