0424beeed622f11664ff6aa007a8ab1c3b00f975
blank2121 <winstonwalter07@gmail.com>
2025-01-28 15:38:38 -0500

    added cmp

--- code/plugins/default.nix
+++ code/plugins/default.nix
@@ -15,18 +15,30 @@
     markview.enable = true;
     twilight.enable = true;
     vim-css-color.enable = true;
-
+    
     cmp = {
         enable = true;
         autoEnableSources = true;
-        # settings.sources = [
-        #   { name = "buffer"; }
-        #   { name = "dictionary"; }
-        #   { name = "latex_symbols"; }
-        #   { name = "nvim-lsp"; }
-        #   { name = "path"; }
-        # ];
+        settings = {
+            sources = [
+              { name = "buffer"; }
+              { name = "dictionary"; }
+              { name = "latex_symbols"; }
+              { name = "nvim_lsp"; }
+              { name = "path"; }
+            ];
+            mapping = {
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-e>" = "cmp.mapping.close()";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+              "<CR>" = "cmp.mapping.confirm({ select = true })";
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            };
+        };
     };
+    
     cmp-buffer.enable = true;
     cmp-dictionary.enable = true;
     cmp-latex-symbols.enable = true;
--- writer/plugins/default.nix
+++ writer/plugins/default.nix
@@ -17,14 +17,33 @@
     };
     vim-css-color.enable = true;
 
-    cmp.enable = true;
-    cmp.autoEnableSources = true;
-    cmp.settings.sources = [
-      { name = "nvim_lsp"; }
-      { name = "path"; }
-      { name = "buffer"; }
-    ];
+    cmp = {
+        enable = true;
+        autoEnableSources = true;
+        settings = {
+            sources = [
+              { name = "buffer"; }
+              { name = "dictionary"; }
+              { name = "latex_symbols"; }
+              { name = "nvim_lsp"; }
+              { name = "path"; }
+            ];
+            mapping = {
+              "<C-Space>" = "cmp.mapping.complete()";
+              "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+              "<C-e>" = "cmp.mapping.close()";
+              "<C-f>" = "cmp.mapping.scroll_docs(4)";
+              "<CR>" = "cmp.mapping.confirm({ select = true })";
+              "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+              "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
+            };
+        };
+    };
+    
+    cmp-buffer.enable = true;
     cmp-dictionary.enable = true;
     cmp-latex-symbols.enable = true;
+    cmp-nvim-lsp.enable = true;
+    cmp-path.enable = true; 
   };
 }
