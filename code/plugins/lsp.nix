16825ef21b8204ac5c1114bfe9aa567c84a92b3f
blank2121 <winstonwalter07@gmail.com>
2025-02-20 11:01:41 -0500

    added zig

--- code/plugins/lsp.nix
+++ code/plugins/lsp.nix
@@ -19,7 +19,7 @@
       ltex.enable = true;
       lua_ls.enable = true;
       marksman.enable = true;
-      nim_langserver.enable = true;
+      nimls.enable = true;
       nixd.enable = true;
       pyright.enable = true;
 
@@ -33,12 +33,17 @@
       texlab.enable = true;
 
       ts_ls.enable = true;
+
+      zls.enable = true;
     };
   };
+  
+  plugins.zig.enable = true;
 
   plugins.lsp-format.enable = true;
   plugins.lspkind.enable = true;
   plugins.lspsaga.enable = true;
   plugins.luasnip.enable = true;
   plugins.telescope.enable = true;
+  plugins.treesitter.enable = true; 
 }
