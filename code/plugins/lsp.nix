f92754717b1b91908e9d00f29cfd571e01829308
blank2121 <winstonwalter07@gmail.com>
2025-03-02 00:12:22 -0500

    rm nim

--- code/plugins/lsp.nix
+++ code/plugins/lsp.nix
@@ -19,7 +19,6 @@
       ltex.enable = true;
       lua_ls.enable = true;
       marksman.enable = true;
-      nimls.enable = true;
       nixd.enable = true;
       pyright.enable = true;
 
@@ -33,17 +32,16 @@
       texlab.enable = true;
 
       ts_ls.enable = true;
-
-      zls.enable = true;
     };
   };
   
-  plugins.zig.enable = true;
-
   plugins.lsp-format.enable = true;
   plugins.lspkind.enable = true;
   plugins.lspsaga.enable = true;
   plugins.luasnip.enable = true;
   plugins.telescope.enable = true;
-  plugins.treesitter.enable = true; 
+
+  plugins.fidget.enable = true;
+  plugins.treesitter.enable = true;
+  plugins.haskell-scope-highlighting.enable = true;
 }
